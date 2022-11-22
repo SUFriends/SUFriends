81e36f7b609e50521998c25b8779bd2d9ef43665
ykc <ykilicarslan@sabanciuniv.edu>
2022-11-22 23:19:00 +0300

    governanceToken

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,6 +1,8 @@
 // migrations/2_deploy.js
 const Box = artifacts.require('Box');
+const GovernanceToken = artifacts.require('GovernanceToken');
 
 module.exports = async function (deployer) {
   await deployer.deploy(Box);
+  await deployer.deploy(GovernanceToken);
 };
