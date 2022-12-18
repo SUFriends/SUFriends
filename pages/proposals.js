4f7309a4f55c169d02aa6b3fa2344340821d6164
dogkan-spiky <dogkan@spiky.ai>
2022-12-18 03:45:07 +0300

    Add proposal card component. Add create proposal button.

--- pages/proposals.js
+++ pages/proposals.js
@@ -1,37 +1,24 @@
 import * as React from 'react';
-import { Card, Avatar, Chip, Typography, Stack } from '@mui/material';
+import { Card, Avatar, Chip, Typography, Stack, Button, Grid } from '@mui/material';
 import Layout from '../components/layouts/layout'
 import { lightGreen } from '@mui/material/colors';
-
+import ProposalCard from '../components/ProposalCard'
 
 
 function Proposals(props) {
   return (
     <Layout>
-      <Card sx={{ p: 2 }}>
-        <Stack direction="row" spacing={3} alignItems="center">
-          <Avatar sx={{ bgcolor: lightGreen[300] }}> </Avatar>
-          <Typography variant='h6'>should we implement this?</Typography>
-          <Chip label="In progress" size='small' color="warning"></Chip>
-          <Typography variant='caption'> ID: {"810ef9...8ac3"}</Typography>
-        </Stack>
-      </Card>
-      <Card sx={{ p: 2 }}>
-        <Stack direction="row" spacing={3} alignItems="center">
-          <Avatar sx={{ bgcolor: lightGreen[300] }}> </Avatar>
-          <Typography variant='h6'>should we implement this?</Typography>
-          <Chip label="Accepted" size='small' color="success"></Chip>
-          <Typography variant='caption'> ID: {"810ef9...8ac3"}</Typography>
-        </Stack>
-      </Card>
-      <Card sx={{ p: 2 }}>
-        <Stack direction="row" spacing={3} alignItems="center">
-          <Avatar sx={{ bgcolor: lightGreen[300] }}> </Avatar>
-          <Typography variant='h6'>should we implement this?</Typography>
-          <Chip label="Denied" size='small' color="error"></Chip>
-          <Typography variant='caption'> ID: {"810ef9...8ac3"}</Typography>
-        </Stack>
-      </Card>
+      <Grid container justifyContent="flex-end">
+        <Button
+          variant='outlined'
+          disableElevation
+        >
+          Create new proposal
+        </Button>
+      </Grid>
+      <ProposalCard title="test" id="testtt" status="inProgress"></ProposalCard>
+      <ProposalCard title="test" id="testtt" status="accepted"></ProposalCard>
+      <ProposalCard title="test" id="testtt" status="denied"></ProposalCard>
     </Layout >
   );
 }
