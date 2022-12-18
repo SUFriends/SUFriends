17ebae9d7228601aaacca36d0db9df7215438261
dogkan-spiky <dogkan@spiky.ai>
2022-12-18 15:49:54 +0300

    Add treasury balance and owners

--- pages/treasury.js
+++ pages/treasury.js
@@ -1,16 +1,19 @@
-import * as React from 'react';
-import { styled, AppBar, Box, Toolbar, Container, Typography, Button, Tabs, Tab } from '@mui/material';
-import Layout from '../components/layouts/layout'
-
-
+import * as React from "react";
+import { Typography } from "@mui/material";
+import Layout from "../components/layouts/layout";
+import MemberCard from "../components/MemberCard";
 
 function Treasury(props) {
-
   return (
-    <Layout>    <Typography>
-      treasury
-    </Typography></Layout>
-
+    <Layout>
+      <Typography variant="h6">Total Balance</Typography>
+      <Typography variant="h4">{"0.2"} ETH</Typography>
+      <Typography variant="h6" sx={{mt: 2}}>Owners</Typography>
+      <MemberCard
+        id="0x57d39B2a3d9Ea14062856388BaF34a6AC17D05fa"
+        proposalsVoted="1"
+      />
+    </Layout>
   );
 }
 
