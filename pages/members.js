1aaee8a76fc2c831b741acccc4369a65c598f45b
dogkan-spiky <dogkan@spiky.ai>
2022-12-18 15:34:36 +0300

    Add member card

--- pages/members.js
+++ pages/members.js
@@ -1,25 +1,15 @@
-import * as React from 'react';
-import { Avatar, Typography, Card, Stack } from '@mui/material';
-import Layout from '../components/layouts/layout'
-import { red } from '@mui/material/colors';
-
-
+import * as React from "react";
+import Layout from "../components/layouts/layout";
+import MemberCard from "../components/MemberCard";
 
 function Members(props) {
   return (
     <Layout>
-      <Card sx={{ p: 2 }}>
-        <Stack direction="row" justifyContent="space-between" alignItems="center">
-          <Stack direction="row" spacing={3} alignItems="center">
-            <Typography>1</Typography>
-            <Avatar sx={{ bgcolor: red[300] }}> </Avatar>
-            <Typography> {"0x57d39B2a3d9Ea14062856388BaF34a6AC17D05fa"}</Typography>
-          </Stack>
-          <Typography> Proposals voted: {"1"}</Typography>
-        </Stack>
-      </Card>
+      <MemberCard
+        id="0x57d39B2a3d9Ea14062856388BaF34a6AC17D05fa"
+        proposalsVoted="1"
+      />
     </Layout>
-
   );
 }
 
