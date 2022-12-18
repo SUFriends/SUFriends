95993430d3c723bee4d4b8d7599c4ca025d08db0
dogkan-spiky <dogkan@spiky.ai>
2022-12-18 15:50:06 +0300

    Run eslint

--- components/ProposalCard.js
+++ components/ProposalCard.js
@@ -1,36 +1,29 @@
-import { Card, Avatar, Chip, Typography, Stack } from '@mui/material';
-import { lightGreen } from '@mui/material/colors';
-
-
-
+import { Card, Avatar, Chip, Typography, Stack } from "@mui/material";
+import { lightGreen } from "@mui/material/colors";
 
 export default function ProposalCard({ title, status, id }) {
-    let color, label;
-    if (status == "inProgress") {
-        color = "warning";
-        label = "In progress"
-    }
-    else if (status == "accepted") {
-        color = "success";
-        label = "Accepted"
-    }
-    else if (status == "denied") {
-        color = "error";
-        label = "Denied"
-    }
+  let color, label;
+  if (status == "inProgress") {
+    color = "warning";
+    label = "In progress";
+  } else if (status == "accepted") {
+    color = "success";
+    label = "Accepted";
+  } else if (status == "denied") {
+    color = "error";
+    label = "Denied";
+  }
 
-    return (
-        <>
-            <Card sx={{ p: 2, my: 2 }}>
-                <Stack direction="row" spacing={3} alignItems="center">
-                    <Avatar sx={{ bgcolor: lightGreen[300] }}> </Avatar>
-                    <Typography variant='h6'>{title}</Typography>
-                    <Chip label={label}
-                        color={color}
-                        size='small' />
-                    <Typography variant='caption'> ID: {id}</Typography>
-                </Stack>
-            </Card>
-        </>
-    );
-} 
+  return (
+    <>
+      <Card sx={{ p: 2, my: 2 }}>
+        <Stack direction="row" spacing={3} alignItems="center">
+          <Avatar sx={{ bgcolor: lightGreen[300] }}> </Avatar>
+          <Typography variant="h6">{title}</Typography>
+          <Chip label={label} color={color} size="small" />
+          <Typography variant="caption"> ID: {id}</Typography>
+        </Stack>
+      </Card>
+    </>
+  );
+}
--- pages/proposals.js
+++ pages/proposals.js
@@ -1,25 +1,20 @@
-import * as React from 'react';
-import { Card, Avatar, Chip, Typography, Stack, Button, Grid } from '@mui/material';
-import Layout from '../components/layouts/layout'
-import { lightGreen } from '@mui/material/colors';
-import ProposalCard from '../components/ProposalCard'
-
+import * as React from "react";
+import { Button, Grid } from "@mui/material";
+import Layout from "../components/layouts/layout";
+import ProposalCard from "../components/ProposalCard";
 
 function Proposals(props) {
   return (
     <Layout>
       <Grid container justifyContent="flex-end">
-        <Button
-          variant='outlined'
-          disableElevation
-        >
+        <Button variant="outlined" disableElevation>
           Create new proposal
         </Button>
       </Grid>
       <ProposalCard title="test" id="testtt" status="inProgress"></ProposalCard>
       <ProposalCard title="test" id="testtt" status="accepted"></ProposalCard>
       <ProposalCard title="test" id="testtt" status="denied"></ProposalCard>
-    </Layout >
+    </Layout>
   );
 }
 
