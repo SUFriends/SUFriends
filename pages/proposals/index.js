4f960f57b76e56c91702f095ff8d438e3f76ec66
dogkan-spiky <dogkan@spiky.ai>
2022-12-31 15:42:41 +0300

    feat: Add dummy modal view to create new proposal

--- pages/proposals/index.js
+++ pages/proposals/index.js
@@ -1,13 +1,41 @@
-import * as React from "react";
-import { Button, Grid } from "@mui/material";
-import Layout from "../../components/layouts/layout";
+import { useState } from "react";
+import { Button, Grid, Modal, Box, Typography } from "@mui/material";
 import ProposalCard from "../../components/ProposalCard";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+export default function Proposals(props) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-function Proposals(props) {
   return (
     <>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
       <Grid container justifyContent="flex-end">
-        <Button variant="outlined" disableElevation>
+        <Button onClick={handleOpen} variant="outlined" disableElevation>
           Create new proposal
         </Button>
       </Grid>
@@ -38,6 +66,3 @@ function Proposals(props) {
     </>
   );
 }
-
-export default Proposals;
-// deneme workout
