539a341d9e9c323f2848fe5a9f2713d92bdc0481
Tolga Aydın <tolgaaydin@sabanciuniv.edu>
2023-01-17 16:25:20 +0300

    Reentrancy test and attacker sample contract added

---
+++ contracts/Attacker.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/test-helpers/contracts/Assert.sol";
+
+contract Attacker is Ownable {
+    Treasury public treasury;
+
+    constructor(address _treasury) public {
+        treasury = Treasury(_treasury);
+    }
+
+    function attack() public {
+        // Attacker calls the releaseFunds function
+        treasury.releaseFunds();
+
+        // Attacker calls the attack function again before the first call has completed
+        attack();
+    }
+}
