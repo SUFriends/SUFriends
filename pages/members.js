55ef16488e46864d4d2e305f8bd10dcdba6c8479
Ali Tarık <alimirik55@gmail.com>
2023-01-17 17:02:11 +0300

    add addresses

--- pages/members.js
+++ pages/members.js
@@ -3,11 +3,24 @@ import Layout from "../components/layouts/layout";
 import MemberCard from "../components/MemberCard";
 
 function Members(props) {
+
+  function getMembers() {
+    const { ethereum } = window;
+    const contract = getContract(contract_address, abi, library, address);
+
+  }
+
+  useEffect(() => {
+    getMembers();
+  }, []);
+
   return (
-    <MemberCard
-      id="0x57d39B2a3d9Ea14062856388BaF34a6AC17D05fa"
-      proposalsVoted="1"
-    />
+    <>
+      <MemberCard
+        id="0x57d39B2a3d9Ea14062856388BaF34a6AC17D05fa"
+        proposalsVoted="1"
+      />
+    </>
   );
 }
 
